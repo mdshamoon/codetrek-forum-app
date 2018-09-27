506239f1d316849d2226c4a99fe5f8693100e684
mdshamoon <mohdshamoon841@gmail.com>
2018-09-27 15:54:05 +0530

    fefe

---
+++ index.php
@@ -0,0 +1,115 @@
+
+<!DOCTYPE html>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+
+    <title>Codetrek</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    
+  </head>
+
+  <body>
+
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+      <a class="navbar-brand" href="#">CODETREK FORUM </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Questions </a>
+          </li>
+
+          
+        
+
+        
+        </ul>
+
+          <li class="nav-item dropdown " style="list-style-type: none;">
+            <a class="nav-link dropdown-toggle" href="" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="color: white; margin-right:  100px;"><i class="fas fa-cog"></i></a>
+            <div class="dropdown-menu" aria-labelledby="dropdown01">
+              <a class="dropdown-item" href="#">Action</a>
+              <a class="dropdown-item" href="#">Another action</a>
+              <a class="dropdown-item" href="#">Something else</a>
+            </div>
+          </li>
+
+      
+       <!--   -->
+        
+       
+         
+     
+      </div>
+    </nav>
+
+<div>
+  <div class="container" style="padding-top: 5rem; overflow: auto;">
+          <h2 class="float-left"> Questions</h2> 
+          <form class="form-inline my-2 my-lg-0 float-right">
+          <input class="form-control mr-sm-2" type="text" placeholder="Search " aria-label="Search">
+          <button class="btn btn-primary mr-2"><i class="fas fa-search "></i></button>
+          <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Ask Questions</button>
+        </form>
+        </div>
+      </div>
+
+
+        <div class="container border mt-4 p-4">
+          
+         <div class="ml-4" >
+           <h5>How do I use Git and Github?</h5>
+           <span>A paragraph is a self-contained unit of a discourse in writing dealing with a particular point or idea. A paragraph consists of one or more sentences. Though not required by the syntax of any language, paragraphs are usually an expected part of formal writing, used to organize longer prose.</span>
+           <div class="mt-3"><span style="color: blue">Mohd Shamoon</span> asked on September 27,2018</div>
+            <div class="mt-3"><i class="far fa-thumbs-up mr-1">14</i><i class="far fa-thumbs-down"></i>18<i class="far fa-comments pl-1"> 18 Answers</i></div>
+        </div>
+         </div>
+
+
+          <div class="container border mt-4 p-4">
+          
+         <div class="ml-4" >
+           <h5>What will i learn from codetrek?</h5>
+           <span>A paragraph is a self-contained unit of a discourse in writing dealing with a particular point or idea. A paragraph consists of one or more sentences. Though not required by the syntax of any language, paragraphs are usually an expected part of formal writing, used to organize longer prose.</span>
+           <div class="mt-3"><span style="color: blue">Aditya Sharma</span> asked on September 27,2018</div>
+            <div class="mt-3"><i class="far fa-thumbs-up mr-1">14</i><i class="far fa-thumbs-down"></i>18<i class="far fa-comments pl-1"> 18 Answers</i></div>
+        </div>
+         </div>
+
+
+          <div class="container border mt-4 p-4">
+          
+         <div class="ml-4" >
+           <h5>what is bootstrap?</h5>
+           <span>A paragraph is a self-contained unit of a discourse in writing dealing with a particular point or idea. A paragraph consists of one or more sentences. Though not required by the syntax of any language, paragraphs are usually an expected part of formal writing, used to organize longer prose.</span>
+           <div class="mt-3"><span style="color: blue">Ritik kumar</span> asked on September 27,2018</div>
+            <div class="mt-3"><i class="far fa-thumbs-up mr-1">14</i><i class="far fa-thumbs-down"></i>18<i class="far fa-comments pl-1"> 18 Answers</i></div>
+        </div>
+         </div>
+
+
+
+
+<footer class="text-center my-3" style="color:blue;"><i class="fas fa-code"></i>Developed at Codetrek, New Tehri</footer>
+
+  
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+  
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
